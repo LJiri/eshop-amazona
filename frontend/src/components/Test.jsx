a5e89bdd37eda5cbb10fe5eab618430ea22e7570
Jiří Laudát <j.laudat@email.cz>
2021-01-03 20:05:47 +0100

    webpack setting

---
+++ frontend/src/components/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import webpackImage from "../assets/webpack.png"
+
+export default function Test() {
+    return (
+        <div>
+            <h1 className="h1">Hello world!</h1>
+            <img src={webpackImage} alt=""/>
+        </div>
+    )
+}
+
---
+++ frontend/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles/index.scss";
+import Test from "./components/Test";
+
+ReactDOM.render(<Test />, document.getElementById("react-app"));
